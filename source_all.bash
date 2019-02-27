def5f70f742614fdf7ea025b6d32d48b7a1e67a4
mariogianni <mario.gianni@plymouth.ac.uk>
2019-02-27 19:38:43 +0000

    update repo

---
+++ source_all.bash
@@ -0,0 +1,11 @@
+#! /bin/bash 
+
+TURTLEBOT_DRIVERS_WS=~/turtlebot2_wss/turtlebot2_ws
+TURTLEBOT_SIMULATION_WS=~/turtlebot2_wss/turtlebot2_simulation_ws
+TURTLEBOT_INTERACTION_WS=~/turtlebot2_wss/turtlebot2_interactions_ws
+
+source $TURTLEBOT_DRIVERS_WS/devel/setup.bash --extend
+source $TURTLEBOT_SIMULATION_WS/devel/setup.bash --extend
+source $TURTLEBOT_INTERACTION_WS/devel/setup.bash --extend
+
+
